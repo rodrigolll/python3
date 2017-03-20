28e35df4bd60ab350df2b393e7466a230761ade1
rodrigolll <hkhaoh@naver.com>
2017-03-20 09:11:19 +0900

    Add files via upload

---
+++ for3.py
@@ -0,0 +1,5 @@
+ingredients = ['snail', 'leeches', 'gorilla belly-button lint', 'caterpillar eyebrows', 'centipede toes']
+a = 0 
+for i in ingredients:
+ a = a + 1
+ print('%s %s' %(a, i))
---
+++ for4.py
@@ -0,0 +1,4 @@
+me = 68
+for i in range(15):
+ me  = me + 1
+ print('%s년 %.2f kg' %(i+1,(me*0.165)))
